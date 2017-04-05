1d9cd6ef1c5da606287a395625bc9d9359ec1776
Marcus Lester <mlester3@cnm.edu>
2017-04-05 17:56:36 -0600

    practiced making a repository, using lists elements, link and title attributes.

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>My test page</title>
+	</head>
+	<body>
+		<h1>Hi There!</h1>
+		<p>This is my page</p>
+		<h2>
+			Reasons
+			<em>
+				Why
+			</em>
+			I made this site.
+		</h2>
+		<ol>
+			<li>
+				<a href="https://toddmooney.files.wordpress.com/2014/05/i-want-to.jpg"
+				title="And this is just the beggining">
+					<strong>Because...</strong>
+				</a>.
+			</li>
+			<li>You can't
+				<a href="http://2.bp.blogspot.com/_Ps08I2eEpgQ/TRc7MSzLFQI/AAAAAAAAACI/sEeN7NsTgpY/S850-R/nmw_logo_bw_xl%2Bwhite.jpg"
+				title="Even if you try">
+					stop me.
+				</a>
+			</li>
+			<li>
+				<a href="https://cdn.meme.am/instances/400x/65031544.jpg"
+				title="Take the brown pill."> Na na na boo boo</a>
+			</li>
+		</ol>
+
+
+
+	</body>
+</html>
